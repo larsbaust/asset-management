492f6865d271e6e7cb09792fa0b9c08b2271a7c7
Lars <baust.lars@gmail.com>
2025-04-15 10:25:09 +0200

    wsgi.py angepasst: Beispieländerung

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,14 @@
-from app import create_app
+import sys
+import os
 
+path = '/home/frittenbude/asset-management'
+if path not in sys.path:
+    sys.path.append(path)
+
+from dotenv import load_dotenv
+load_dotenv(os.path.join(path, '.env'))
+
+from app import create_app
 application = create_app()
 
 if __name__ == '__main__':
